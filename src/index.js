bfcd28794d327405c17eb8af0872ad93f43cb695
Amal911 <amaleattath911@gmail.com>
2023-11-21 11:14:08 +0530

    pages edited

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Route, Routes} from 'react-router-dom';
+
+import { Home } from "./pages/Home.jsx";
+import { ProjectsPage } from './pages/Projects.jsx';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+    <Routes>
+      <Route path="/" element ={<Home />} exact/>
+      <Route path='/projects' element={<ProjectsPage />} exact/>
+    </Routes>
     </Router>
    
   </React.StrictMode>
